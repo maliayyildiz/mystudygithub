fdee81a1ad6948ac9ba1273805a38262bde29e80
maliayyildiz <maliayyildiz61@gmail.com>
2020-08-13 05:08:47 +0200

    3rd from localrepo

--- src/mystudygithub/MyStudy.java
+++ src/mystudygithub/MyStudy.java
@@ -5,6 +5,10 @@ public class MyStudy {
 	public static void main(String[] args) {
 		System.out.println("this is my github project and 1st commit");
         System.out.println("2nd commit from local repo");
+        System.out.println("3rd commit from local repo");
+        
+        
+        
 	}
 
 }
