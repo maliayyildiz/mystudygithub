e0a19030b53ce2ee230f18783fcd8773fbc5ca92
maliayyildiz <maliayyildiz61@gmail.com>
2020-08-13 05:34:33 +0200

    conflict maker

--- src/mystudygithub/MyStudy.java
+++ src/mystudygithub/MyStudy.java
@@ -19,6 +19,7 @@ public class MyStudy {
 		System.out.println("1st commit from remote ayyildiz repo");
 		
 		System.out.println("1st commit from local ayyildiz repo");
+		System.out.println("this line vreated for onflict");
 		
 	}
 
