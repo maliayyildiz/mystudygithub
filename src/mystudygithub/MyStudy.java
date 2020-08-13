097dd76d7e9f78d13e6e2d83ac809b5a46333393
maliayyildiz <maliayyildiz61@gmail.com>
2020-08-13 05:07:18 +0200

    2nd commit from local repo

--- src/mystudygithub/MyStudy.java
+++ src/mystudygithub/MyStudy.java
@@ -4,7 +4,7 @@ public class MyStudy {
 
 	public static void main(String[] args) {
 		System.out.println("this is my github project and 1st commit");
-
+        System.out.println("2nd commit from local repo");
 	}
 
 }
